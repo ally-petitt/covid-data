592cc0fdc17ea445f20a19c168805462480dd7b2
ally-petitt <allypetitt@gmail.com>
2021-06-10 07:55:05 -0700

    fixed api call in BarGraph.js

--- client/src/components/BarGraph.js
+++ client/src/components/BarGraph.js
@@ -3,80 +3,43 @@ import axios from "axios"
 import CanvasJSReact from "../lib/canvasjs/canvasjs.react";
 
 const BarGraph = () => {
-    const [isGettingData, setIsGettingData] = useState(true);
+    const [options, setOptions] = useState({});
     var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
     let response;
-    let options;
 
     useEffect(async () => {
-        // const result = await axios.get(`http://localhost:4000/graph/country/italy`).then((res) => {
-        //     response = res.data;
-        //     console.log(res.data)
-        // })
-        // .catch((err) => {
-        //     console.log(err);
-        // })
+        const result = await axios.get(`http://localhost:4000/graph/country/italy`)
+            .then((res) => {
+                response = res.data;
+                console.log(res.data)
+            })
+            .catch((err) => {
+                console.log(err);
+            })
 
-        // options = {
-        //     title: {
-        //         // text: `COVID-19 Statistics in ${response.country}`
-        //         text: "test"
-        //     },
-        //     data: [{	
-        //         type: "column",
-        //         dataPoints: [
-        //             {
-        //                 // y: response.confirmed,
-        //                 y: 8,
-        //                 label: "Confirmed"
-        //             }
-        //         ]
-        //     }]
-        // }
-
-        // options = {
-        //     title: {
-        //         text: "Basic Column Chart in React"
-        //     },
-        //     data: [{				
-        //                 type: "column",
-        //                 dataPoints: [
-        //                     { label: "Apple",  y: 10  },
-        //                     { label: "Orange", y: 15  },
-        //                     { label: "Banana", y: 25  },
-        //                     { label: "Mango",  y: 30  },
-        //                     { label: "Grape",  y: 28  }
-        //                 ]
-        //     }]
-        // }
-
-        // almost there
-        setIsGettingData(false);
-        // set false
-    }, []);
-
-    options = {
+        let data = {
             title: {
-                text: "Basic Column Chart in React"
+                // text: `COVID-19 Statistics in ${response.country}`
+                text: "test"
             },
-            data: [{				
-                        type: "column",
-                        dataPoints: [
-                            { label: "Apple",  y: 10  },
-                            { label: "Orange", y: 15  },
-                            { label: "Banana", y: 25  },
-                            { label: "Mango",  y: 30  },
-                            { label: "Grape",  y: 28  }
-                        ]
+            data: [{	
+                type: "column",
+                dataPoints: [
+                    {
+                        y: response.confirmed,
+                        label: "Confirmed"
+                    }
+                ]
             }]
         }
 
-        // the problem has to do with the options not being assigned as the component renders
+        setOptions(data);
+    }, []);
 
     return (
         <div>
-            {/* {!isGettingData ? <CanvasJSChart options={options} /> : null} */}
+            {options == {} ? "nothing": <CanvasJSChart options={options} />}
         </div>
     )
 }
